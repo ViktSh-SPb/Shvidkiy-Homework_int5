641e8c947e3a5717f846e1e7e9c50e2723ba6faa
Viktor <vikt.sh@gmail.com>
2025-09-05 23:08:14 +0300

    Добавил валидацию и документацию

--- user-service/src/main/java/org/example/user_service/controller/MyExceptionHandler.java
+++ user-service/src/main/java/org/example/user_service/controller/MyExceptionHandler.java
@@ -1,10 +1,15 @@
 package org.example.user_service.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.example.user_service.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Viktor Shvidkiy
@@ -17,5 +22,14 @@ public class MyExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error->errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
--- user-service/src/main/java/org/example/user_service/controller/UserController.java
+++ user-service/src/main/java/org/example/user_service/controller/UserController.java
@@ -1,5 +1,13 @@
 package org.example.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.example.user_service.dto.UserDto;
 import org.example.user_service.dto.UserRequestDto;
 import org.example.user_service.service.UserService;
@@ -11,6 +19,7 @@ import java.util.List;
 /**
  * @author Viktor Shvidkiy
  */
+@Tag(name = "Users", description = "Операции с пользователями")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,29 +29,108 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Создать нового пользователя")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Пользователь успешно создан",
+                    content = @Content(schema = @Schema(implementation = UserDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректные данные",
+                    content = @Content(schema = @Schema(
+                            type = "object",
+                            additionalPropertiesSchema = String.class,
+                            example = "{\"name\":\"Имя не может быть пустым\", \"email\":\"Некорректный email\", \"age\":\"Возраст не может быть отрицательным\"}"
+                    ))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content
+            )
+    })
     @PostMapping
-    public UserDto createUser(@RequestBody UserRequestDto dto){
+    public UserDto createUser(@Valid @RequestBody UserRequestDto dto) {
         return userService.createUser(dto);
     }
 
+    @Operation(summary = "Получить список всех пользователей")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Список пользователей получен",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDto.class)))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content
+            )
+    })
     @GetMapping
-    public List<UserDto> getAllUsers(){
+    public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Найти пользователя по ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Пользователь найден",
+                    content = @Content(schema = @Schema(implementation = UserDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Пользователь не найден",
+                    content = @Content(schema = @Schema(
+                            type = "object",
+                            additionalPropertiesSchema = String.class,
+                            example = "{\"name\":\"Имя не может быть пустым\", \"email\":\"Некорректный email\", \"age\":\"Возраст не может быть отрицательным\"}"
+                    ))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content
+            )
+    })
     @GetMapping("/{id}")
-    public UserDto getUserById(@PathVariable Integer id){
+    public UserDto getUserById(@PathVariable Integer id) {
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "Обновить данные пользователя")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Данные успешно обновлены",
+                    content = @Content(schema = @Schema(implementation = UserDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректные данные",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content
+            )
+    })
     @PutMapping("/{id}")
-    public UserDto updateUser(@PathVariable Integer id,
-                              @RequestBody UserRequestDto dto){
+    public UserDto updateUser(@PathVariable Integer id, @Valid @RequestBody UserRequestDto dto) {
         return userService.updateUser(id, dto);
     }
 
+    @Operation(summary = "Удалить пользователя")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Пользователь удален"),
+            @ApiResponse(responseCode = "404", description = "Пользователь не найден")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Integer id){
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
