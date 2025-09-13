4d7c5e5cacc261b4ac97d4ef612785192a365d93
Viktor <vikt.sh@gmail.com>
2025-09-13 23:31:23 +0300

    Незначительные исправления

--- user-service/src/main/java/org/example/user_service/controller/MyExceptionHandler.java
+++ user-service/src/main/java/org/example/user_service/controller/MyExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.example.user_service.controller;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.example.user_service.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
